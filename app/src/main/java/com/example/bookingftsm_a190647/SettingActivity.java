c52066f2073c561f3532a3b7e6a553b965bd6b8d
farhana <farhana.miswan@gmail.com>
2023-08-04 15:41:05 +0800

    seventh commit with dark mode and it works!!

--- app/src/main/java/com/example/bookingftsm_a190647/SettingActivity.java
+++ app/src/main/java/com/example/bookingftsm_a190647/SettingActivity.java
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingActivity extends AppCompatActivity {
 
     private Switch switchDarkMode;
+    private Button btnLogout;
     private SharedPreferences sharedPreferences;
     private static final String SHARED_PREFS = "sharedPrefs";
     private static final String DARK_MODE = "isDarkModeOn";
@@ -23,6 +29,7 @@ public class SettingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_setting);
 
         switchDarkMode = findViewById(R.id.switch_darkmode);
+        btnLogout = findViewById(R.id.btn_logout);
         sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
         boolean isDarkModeOn = sharedPreferences.getBoolean(DARK_MODE, false);
         switchDarkMode.setChecked(isDarkModeOn);
@@ -41,6 +48,20 @@ public class SettingActivity extends AppCompatActivity {
                 } else {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 }
+            }
+        });
+
+        // Log out the user when the logout button is clicked
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Log out the user
+                FirebaseAuth.getInstance().signOut();
+
+                // Redirect to LoginActivity
+                Intent intent = new Intent(SettingActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish(); // Finish the SettingActivity to prevent going back to it after logout
             }
         });
     }
